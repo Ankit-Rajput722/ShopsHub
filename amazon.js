a4a73d1a1b79aa553a6e3d0b945d8ab9ca7a29ba
Ankit-Rajput722 <154001296+Ankit-Rajput722@users.noreply.github.com>
2024-01-01 17:17:34 +0530

    Add files via upload

---
+++ amazon.js
@@ -0,0 +1,11 @@
+
+document.getElementById("barimage").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("dropdown").style.display="block";
+}
+document.getElementById("crossmark").onclick = function() {myFunction1()};
+
+function myFunction1() {
+  document.getElementById("dropdown").style.display="none";
+}
